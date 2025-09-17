e1e80d9290cfdf6f5cad0e6c9f3d66daf1218c55
coddey-me <113989495+coddey-me@users.noreply.github.com>
2025-09-17 12:52:13 +0300

    Update train.py

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # === Hyperparameters ===
 maze_size = 10
 batch_size = 32
-epochs = 50
+epochs = 100
 lr = 1e-3
 
 # === Data ===
